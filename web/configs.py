4db98f28f55469e76388a094aff104c7a20cb906
KilluaaaCHEN <killua_cz@163.com>
2023-11-10 11:00:09 +0800

    feat: 菜单

--- web/configs.py
+++ web/configs.py
@@ -15,6 +15,52 @@ def config_list():
     return {'data': {'list': con_list}, 'code': 200}, 200
 
 
+@configs.route('/menus', methods=['GET'])
+def menus_list():
+    menus = [
+        {
+            'name': 'Baas',
+            'text': 'Baas',
+            'child': [
+                {'name': 'baas', 'text': 'Baas设置'},
+                {'name': 'restart', 'text': '重启设置'},
+            ]
+        },
+        {
+            'name': 'daily',
+            'text': '每日',
+            'child': [
+                {'name': 'group', 'text': '小组'},
+                {'name': 'make', 'text': '制造'},
+                {'name': 'schedule', 'text': '日程'},
+                {'name': 'shop', 'text': '商店购买'},
+                {'name': 'cafe', 'text': '咖啡厅'},
+                {'name': 'arena', 'text': '战术对抗赛'},
+                {'name': 'special_entrust', 'text': '特殊委托'},
+                {'name': 'wanted', 'text': '通缉悬赏'},
+            ]
+        },
+        {
+            'name': 'reward',
+            'text': '收获',
+            'child': [
+                {'name': 'mailbox', 'text': '领取邮箱'},
+                {'name': 'momo_talk', 'text': 'MomoTalk'},
+                {'name': 'work_task', 'text': '工作任务'},
+            ]
+        },
+        {
+            'name': 'scan',
+            'text': '出击',
+            'child': [
+                {'name': 'normal_task', 'text': '扫荡普通关卡'},
+                {'name': 'hard_task', 'text': '扫荡困难关卡'},
+            ]
+        },
+    ]
+    return {'data': {'list': menus}, 'code': 200}, 200
+
+
 @configs.route('/configs/<string:con>/<string:fn>', methods=['GET'])
 def config_detail(con, fn):
     data = config.load_ba_config(con)
